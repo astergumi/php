062ba3272786bfcb20abfa086378ac74befbfeb8
Pavel Osipov <154364350+yaosipovpavel@users.noreply.github.com>
2024-02-16 22:06:45 +0500

    Add new buttons, change id for buttons. Add hover effects for buttons. Create event for numbers buttons

--- bjs/07_Number_and_string/script.js
+++ bjs/07_Number_and_string/script.js
@@ -1,12 +1,52 @@
 let lastOperand = 0;
 let operation = null;
 
-const inputWindow = document.getElementById('inputWindow');
+// Связываем поле input с переменной inputWindow
+const inputWindow = document.querySelector('#inputWindow');
 
-
-document.getElementById('btn_clr').addEventListener('click', function () {
+//При нажатии на кнопку #btn_clr, очищаем значения в input
+document.querySelector('#btn_clr').addEventListener('click', function () {
     lastOperand = 0;
     operation = null;
     inputWindow.value = '';
 })
 
+document.querySelector('#btn_one').addEventListener('click', function () {
+    inputWindow.value += '1';
+})
+
+document.querySelector('#btn_second').addEventListener('click', function () {
+    inputWindow.value += '2';
+})
+
+document.querySelector('#btn_three').addEventListener('click', function () {
+    inputWindow.value += '3';
+})
+
+document.querySelector('#btn_four').addEventListener('click', function () {
+    inputWindow.value += '4';
+})
+
+document.querySelector('#btn_five').addEventListener('click', function () {
+    inputWindow.value += '5';
+})
+
+document.querySelector('#btn_six').addEventListener('click', function () {
+    inputWindow.value += '6';
+})
+
+document.querySelector('#btn_seven').addEventListener('click', function () {
+    inputWindow.value += '7';
+})
+
+document.querySelector('#btn_eight').addEventListener('click', function () {
+    inputWindow.value += '8';
+})
+
+document.querySelector('#btn_nine').addEventListener('click', function () {
+    inputWindow.value += '9';
+})
+
+document.querySelector('#btn_zero').addEventListener('click', function () {
+    inputWindow.value += '0';
+})
