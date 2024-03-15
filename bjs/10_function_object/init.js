590d2dd08e5a91848e31d862f12b6db7f180f7dc
Pavel Osipov <154364350+yaosipovpavel@users.noreply.github.com>
2024-03-15 20:45:13 +0500

    Add file style css. Update files personGenerator.js and init.js. Add fonts 'Netflix.ttf'

--- bjs/10_function_object/init.js
+++ bjs/10_function_object/init.js
@@ -1,10 +1,22 @@
-
-window.onload = function()
-{
+window.onload = function () {
     const initPerson = personGenerator.getPerson();
     document.getElementById('firstNameOutput').innerText = initPerson.firstName;
     document.getElementById('genderOutput').innerText = initPerson.gender;
+    document.getElementById('Patronymic').innerText = initPerson.patronymic;
     document.getElementById('surnameOutput').innerText = initPerson.surName;
+    document.getElementById('nameProfession').innerText = initPerson.Profession;
     document.getElementById('birthYearOutput').innerText = initPerson.YearDate;
-};
 
+    document.getElementById('btnGenerate').addEventListener('click', function () {
+        window.onload();
+    })
+
+    document.getElementById('btnClear').addEventListener('click', function (event) {
+        document.getElementById('firstNameOutput').innerText = '';
+        document.getElementById('genderOutput').innerText = '';
+        document.getElementById('surnameOutput').innerText = '';
+        document.getElementById('nameProfession').innerText = '';
+        document.getElementById('birthYearOutput').innerText = '';
+        document.getElementById('Patronymic').innerText = '';
+    })
+};
--- bjs/10_function_object/personGenerator.js
+++ bjs/10_function_object/personGenerator.js
@@ -35,6 +35,21 @@ const personGenerator = {
             "id_10": "Андрей"
         }
     }`,
+    jobsMaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Программист",
+            "id_2": "Охранник",
+            "id_3": "Полицейский",
+            "id_4": "Врач",
+            "id_5": "Механик",
+            "id_6": "Пожарный",
+            "id_7": "Тренер",
+            "id_8": "Водитель",
+            "id_9": "Гид",
+            "id_10": "Пилот"
+        }
+    }`,
     firstNameFeMaleJson: `{
         "count": 10,
         "list": {     
@@ -50,7 +65,39 @@ const personGenerator = {
             "id_10": "Ирина"
         }
     }`,
-    
+    jobsFeMaleJson: `{
+        "count": 10,
+        "list": {     
+            "id_1": "Администратор",
+            "id_2": "Повар",
+            "id_3": "Архитектор",
+            "id_4": "Дизайнер",
+            "id_5": "Флорист",
+            "id_6": "Продавец",
+            "id_7": "Стилист",
+            "id_8": "Врач",
+            "id_9": "Учитель",
+            "id_10": "Стюардесса"
+        }
+    }`,
+    nameMonthJson: `{
+        "count": 12,
+        "list": {     
+            "id_1": "Января",
+            "id_2": "Февраля",
+            "id_3": "Марта",
+            "id_4": "Апреля",
+            "id_5": "Мая",
+            "id_6": "Июня",
+            "id_7": "Июля",
+            "id_8": "Августа",
+            "id_9": "Сентября",
+            "id_10": "Октября",
+            "id_11": "Ноября",
+            "id_12": "Декабря"
+        }
+    }`,
+
 
     GENDER_MALE: 'Мужчина',
     GENDER_FEMALE: 'Женщина',
@@ -59,43 +106,104 @@ const personGenerator = {
 
     randomValue: function (json) {
         const obj = JSON.parse(json);
-        const prop = `id_${this.randomIntNumber(obj.count, 1)}`;  // this = personGenerator
+        const prop = `id_${this.randomIntNumber(obj.count, 1)}`; // this = personGenerator
         return obj.list[prop];
     },
 
-    randomFirstName: function() {
+    randomSurname: function () {
 
-        if(gender === this.GENDER_MALE){
-            return this.randomValue(this.firstNameMaleJson);
+        if (gender === this.GENDER_FEMALE)
+            return this.randomValue(this.surnameJson) + 'а';
+        else {
+            return this.randomValue(this.surnameJson);
         }
-        else if(gender === this.GENDER_FEMALE){
-            return this.randomValue(this.firstNameFeMaleJson);
-        }
-
 
     },
 
-    randomGender: function() {
-         gender = this.randomIntNumber(1, 0) === 0 ? this.GENDER_MALE : this.GENDER_FEMALE;
+    randomGender: function () {
+        gender = this.randomIntNumber(1, 0) === 0 ? this.GENDER_MALE : this.GENDER_FEMALE;
         return gender;
 
     },
-    
 
 
-     randomSurname: function() {
-        
-        if(gender === this.GENDER_FEMALE)
-        return this.randomValue(this.surnameJson)+'а';
-        else{
-        return this.randomValue(this.surnameJson);
-    }
-
+    randomProfession: function () {
+        if (gender === this.GENDER_MALE) {
+            return this.randomValue(this.jobsMaleJson);
+        } else if (gender === this.GENDER_FEMALE) {
+            return this.randomValue(this.jobsFeMaleJson);
+        }
     },
+
+    randomFirstName: function () {
+
+        if (gender === this.GENDER_MALE) {
+            return this.randomValue(this.firstNameMaleJson);
+        } else if (gender === this.GENDER_FEMALE) {
+            return this.randomValue(this.firstNameFeMaleJson);
+        }
+    },
+
+    randomPatronymic: function () {
+        let patronymic = this.randomValue(this.firstNameMaleJson);
+        return this.namePatronymic(patronymic);
+    },
+
+    namePatronymic: function (name) {
+        if (gender === this.GENDER_MALE) {
+            switch (name) {
+                case "Александр":
+                case "Максим":
+                case "Иван":
+                case "Артем":
+                case "Михаил":
+                case "Даниил":
+                case "Егор":
+                    return name + 'ович';
+                case "Дмитрий":
+                case "Андрей":
+                case "Сергей":
+                    return name.slice(0, -1) + 'евич';
+                case "Никита":
+                    return name.slice(0, -1) + 'ич';
+                case "Павел":
+                    return name.slice(0, -3) + 'лович';
+            }
+        } else if (gender === this.GENDER_FEMALE) {
+            switch (name) {
+                case "Александр":
+                case "Максим":
+                case "Иван":
+                case "Артем":
+                case "Михаил":
+                case "Даниил":
+                case "Егор":
+                    return name + 'овна';
+                case "Дмитрий":
+                case "Андрей":
+                case "Сергей":
+                    return name.slice(0, -1) + 'евна';
+                case "Никита":
+                    return name.slice(0, -1) + 'ична';
+                case "Павел":
+                    return name.slice(0, -3) + 'ловна';
+            }
+        }
+    },
+
+
 
     randomYearDate: function () {
         year = this.randomIntNumber(2001, 1950);
-        date = `${year} г.`;
+        month = this.randomValue(this.nameMonthJson);
+        if (this.month === `id_1` || `id_3` || `id_5` || `id_7` || `id_8` || `id_10` || `id_12`) {
+            day = this.randomIntNumber(31, 1);
+        } else if (this.month === `id_4` || `id_6` || `id_9` || `id_11`) {
+            day = this.randomIntNumber(30, 1);
+        } else {
+            day = this.randomIntNumber(28, 1);
+        }
+        date = `${day} ${month} ${year} г.`;
         return date;
     },
 
@@ -103,6 +211,8 @@ const personGenerator = {
     getPerson: function () {
         this.person = {};
         this.person.gender = this.randomGender();
+        this.person.patronymic = this.randomPatronymic();
+        this.person.Profession = this.randomProfession();
         this.person.firstName = this.randomFirstName();
         this.person.surName = this.randomSurname();
         this.person.YearDate = this.randomYearDate();
